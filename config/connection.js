5ed0b11cf2756a75654e61c7a1a49900be2865a4
Tali Saar <tali@rooster.co>
2017-08-12 11:07:38 -0700

    jawsdb setup

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,31 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+
+  connection = mysql.createConnection({
   port: 3306,
-  host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "mg5mq87repqigbmn",
-  password: "yrx3hyikskr0htdb",
+  host: "localhost",
+  user: "root",
+  password: "",
   database: "burger_db"
 });
+
+};
+
+
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
