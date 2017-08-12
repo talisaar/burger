2d25c95829f14c638f2340d92eb06633920b88d0
Tali Saar <tali@rooster.co>
2017-08-11 18:13:14 -0700

    full app

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+### Schema
+
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devoured boolean NOT NULL DEFAULT false,
+	ts TIMEstamp
+	PRIMARY KEY (id)
+);
