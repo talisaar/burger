54afcc9508b9761f86ff57f533fd3eb529763bea
Tali Saar <tali@rooster.co>
2017-08-11 18:40:55 -0700

    wtf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
